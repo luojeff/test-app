acd4fa1a7003b7dcac5eb73e02c85f2037bc1d29
Jeffrey Luo <jeff@Jeffreys-MBP.cable.rcn.com>
2024-12-07 18:19:00 -0500

    a

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
+
+xx = 10
     
 @app.route("/data", methods=["GET"])
 def index():
-    return "Raymond James"
+    mydict = {"data": x}
+    return json.dumps(mydict)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
