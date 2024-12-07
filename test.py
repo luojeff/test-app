74b19f0e8f69345e00f3e60d5c11d7efb249c5c6
Jeffrey Luo <jeff@Jeffreys-MBP.cable.rcn.com>
2024-12-07 02:01:58 -0500

    done

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-import math
+from flask import Flask
 
-print(math.sin(math.pi/2))
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
