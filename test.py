067e780b63dfa120b68c7419d15190866dfd7332
Jeffrey Luo <jeff@Jeffreys-MBP.cable.rcn.com>
2024-12-08 00:41:37 -0500

    a

--- test.py
+++ test.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
+app.config["nam"] = "Jeffrey"
     
 @app.route("/", methods=["GET", "POST"])
 def index():
-    valu = 0;
     if request.method == "POST":
-        valu = 5
+        if app.config["nam"] = "Jeffrey":
+            app.config["nam"] = "Kelly"
+        else:
+            app.config["nam"] = "Jeffrey"    
     
-    return render_template("index.html", val=valu)
+    return render_template("index.html", name = app.config["nam"])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
