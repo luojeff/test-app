e59fb96e9ec318ca088e5aba42d1d412113ee9c0
Jeffrey Luo <jeff@Jeffreys-MacBook-Pro.local>
2024-12-23 18:52:11 -0800

    .:

--- test.py
+++ test.py
@@ -6,17 +6,24 @@ app = Flask(__name__)
     
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        return "button pressed"
-    
+    print(request.method)
     return render_template("index.html")
 
 @app.route("/query", methods=["GET", "POST"])
 def query():
     conn = get_db_connection()
     cur = conn.cursor()
+
+    print (request.method)
+
+    if request.method == 'POST':
+        cur.execute("INSERT INTO test_table(name, age) VALUES (%s, %s);", (request.form.get("name"), request.form.get("age")))
+        conn.commit()
+        print("Post executed!")
+
     cur.execute("SELECT * FROM test_table;")
     data = cur.fetchall()
+
     cur.close()
     conn.close()
 
